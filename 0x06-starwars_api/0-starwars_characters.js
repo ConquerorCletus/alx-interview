2b7f9dd1d07bd00783bebd47df7258dd26e39414
ConquerorCletus <smartchibuike2019@gmail.com>
2024-01-18 22:48:51 +0100

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,43 @@
 #!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+
+if (!movieId) {
+    console.error('Usage: ./0-starwars_characters.js <movieId>');
+    process.exit(1);
+}
+
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+        process.exit(1);
+    }
+
+    if (response.statusCode !== 200) {
+        console.error('Failed to retrieve movie information. Status code:', response.statusCode);
+        process.exit(1);
+    }
+
+    const movieData = JSON.parse(body);
+    const castUrls = movieData.characters;
+
+    // Make requests to character URLs to get names
+    castUrls.forEach(characterUrl => {
+        request(characterUrl, (error, response, body) => {
+            if (error) {
+                console.error('Error:', error);
+                process.exit(1);
+            }
+
+            if (response.statusCode !== 200) {
+                console.error('Failed to retrieve character information. Status code:', response.statusCode);
+                process.exit(1);
+            }
+
+            const characterData = JSON.parse(body);
+            console.log(characterData.name);
+        });
+    });
+});
