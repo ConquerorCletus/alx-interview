e113a7bde0def6fa6198eade4f43edef2ebfb4ca
ConquerorCletus <smartchibuike2019@gmail.com>
2024-01-20 19:56:02 +0100

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,33 +3,33 @@ const request = require('request');
 const movieId = process.argv[2];
 
 if (!movieId) {
-    console.error('Usage: ./0-starwars_characters.js <movieId>');
-    process.exit(1);
+  console.error('Usage: ./0-starwars_characters.js <movieId>');
+  process.exit(1);
 }
 
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 function apiRequest (cast, index) {
-    if (cast.length === index) {
-      return;
-    }
-  
-    request(cast[index], (error, response, body) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log(JSON.parse(body).name);
-        apiRequest(cast, index + 1);
-      }
-    });
+  if (cast.length === index) {
+    return;
   }
-  
-  request(url, (error, response, body) => {
+
+  request(cast[index], (error, response, body) => {
     if (error) {
       console.log(error);
     } else {
-      const cast = JSON.parse(body).characters;
-  
-      apiRequest(cast, 0);
+      console.log(JSON.parse(body).name);
+      apiRequest(cast, index + 1);
     }
   });
+}
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const cast = JSON.parse(body).characters;
+
+    apiRequest(cast, 0);
+  }
+});
