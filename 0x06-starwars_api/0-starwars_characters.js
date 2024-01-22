89a63616cf081d2eb50d61aabb9f52bc76d5c2dd
ConquerorCletus <smartchibuike2019@gmail.com>
2024-01-22 03:36:00 +0100

    new starwarsAPI

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,35 +6,49 @@ if (!movieId) {
   console.error('Usage: ./0-starwars_characters.js <movieId>');
   process.exit(1);
 }
-const url = `https:swapi-api.alx-tools.com/api/films/${movieId}/`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 let people = [];
 const filmCast = [];
 
 const requestCharacters = async () => {
-  await new Promise(resolve => request(url, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
-    }
-  }));
+  try {
+    await new Promise((resolve, reject) => {
+      request(url, (err, res, body) => {
+        if (err || (res && res.statusCode !== 200)) {
+          console.error('Error: ', err, '| StatusCode: ', res ? res.statusCode : 'unknown');
+          reject(err);
+        } else {
+          const jsonBody = JSON.parse(body);
+          people = jsonBody.characters;
+          resolve();
+        }
+      });
+    });
+  } catch (error) {
+    console.error('Error requesting characters: ', error);
+  }
 };
 
 const requestNames = async () => {
   if (people.length > 0) {
     for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jsonBody = JSON.parse(body);
-          filmCast.push(jsonBody.name);
-          resolve();
-        }
-      }));
+      try {
+        await new Promise((resolve, reject) => {
+          request(p, (err, res, body) => {
+            if (err || (res && res.statusCode !== 200)) {
+              console.error('Error: ', err, '| StatusCode: ', res ? res.statusCode : 'unknown');
+              reject(err);
+            } else {
+              const jsonBody = JSON.parse(body);
+              filmCast.push(jsonBody.name);
+              resolve();
+            }
+          });
+        });
+      } catch (error) {
+        console.error('Error requesting names: ', error);
+      }
     }
   } else {
     console.error('Error: Got no Characters for some reason');
@@ -42,15 +56,19 @@ const requestNames = async () => {
 };
 
 const getCast = async () => {
-  await requestCharacters();
-  await requestNames();
+  try {
+    await requestCharacters();
+    await requestNames();
 
-  for (const i of filmCast) {
-    if (i === filmCast[filmCast.length - 1]) {
-      process.stdout.write(i);
-    } else {
-      process.stdout.write(i + '\n');
+    for (const i of filmCast) {
+      if (i === filmCast[filmCast.length - 1]) {
+        process.stdout.write(i);
+      } else {
+        process.stdout.write(i + '\n');
+      }
     }
+  } catch (error) {
+    console.error('Error: ', error);
   }
 };
 
